f62bf6fcfc07bb562da809ce62393a1cba06e0f5
u16suzu <u16suzu@gmail.com>
2014-05-21 16:32:51 +0900

    Implement table view controller.

---
+++ StoryBoardSpike/HogeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HogeTableViewController.h
+//  StoryBoardSpike
+//
+//  Created by u16suzu on 2014/05/21.
+//  Copyright (c) 2014年 IGNIS LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HogeTableViewController : UITableViewController
+
+@end
